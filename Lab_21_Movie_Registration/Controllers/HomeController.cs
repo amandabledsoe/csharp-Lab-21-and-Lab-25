e614db611e9f6aa1547eed9525e83c6f1b603cd6
Amanda <amandajbledsoe@gmail.com>
2020-05-21 08:15:53 -0400

    initial Project Upload

---
+++ Lab_21_Movie_Registration/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_21_Movie_Registration.Models;
+
+namespace Lab_21_Movie_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GetMovieDetails()
+        {
+            return View();
+        }
+
+        public IActionResult ShowMovieResults(MovieModel movie)
+        {
+            return View(movie);
+        }
+
+        public IActionResult ShowMovieResultSummary(MovieModel movie)
+        {
+            return View(movie);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_21_Movie_Registration/Models/MovieModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_21_Movie_Registration.Models
+{
+    public class MovieModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+        public MovieModel()
+        {
+
+        }
+
+        public MovieModel(int ID, string Title, string Genre, int Year, string Actors, string Directors)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.Genre = Genre;
+            this.Year = Year;
+            this.Actors = Actors;
+            this.Directors = Directors;
+        }
+    }
+}
